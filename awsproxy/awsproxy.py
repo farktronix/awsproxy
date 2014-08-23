ef627263dc7745b4984c7af818f1bbd90b1ef86a
Jacob Farkas <jacobf@rkas.net>
2014-08-23 13:52:38 -0700

    Adding some more setup code. I wish there were an easier way to do this, and I don't want to write a general AWS configuration framework

--- awsproxy/awsproxy.py
+++ awsproxy/awsproxy.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 
 import boto.ec2
+import boto.vpc
 
-# Configuration
+###
+## User Configuration
+###
 default_region="us-east-1"
 
-# Config stuff you probably don't need to change
+###
+## Config stuff you probably don't need to change
+###
 awsPublicKeyPath="~/.awsProxy.pub"
 awsPrivateKeyPath="~/.awsProxy"
 
-# Code happens here
+###
+## Code happens here. You shouldn't need to change anything below here
+###
+
+###
+## Constants
+###
 ec2StatePending = 0
 ec2StateRunning = 16
 ec2StateShuttingDown = 32
@@ -27,14 +38,90 @@ AWSProxyKey = "AWSProxy"
 userData = "#!/bin/bash\
 sudo apt-get install tinyproxy"
 
-def getProxyInstance(ec2):
-    instances = ec2.get_only_instances()
+###
+## Finding Instances
+### 
+
+def findProxyInstance(instances):
     retval = None
-    for instance in instances:
-        if instance.tags.get(AWSProxyKey) is not None:
-            retval = instance
+    if instances is not None:
+        for instance in instances:
+            if instance.tags.get(AWSProxyKey) is not None:
+                retval = instance
+                break
+    return retval
+    
+def findExistingEC2Instance(ec2):
+    return findProxyInstance(ec2.get_only_instances())
+    
+def findExistingSecurityGroup(ec2):
+    return findProxyInstance(vpc.get_all_security_groups())
+    
+def findExistingInternetGateway(vpc):
+    return findProxyInstance(vpc.get_all_internet_gateways())
+    
+def findExistingVPCInstance(vpc):
+    return findProxyInstance(vpc.get_all_vpcs())
+    
+def findExistingSubnet(vpc):
+    return findProxyInstance(vpc.get_all_subnets())
+
+def findExistingRouteTable(vpc):
+    return findProxyInstance(vpc.get_all_route_tables())
+
+def findExistingNetworkACL(vpc):
+    return findProxyInstance(vpc.get_all_network_acls())
+
+###
+## Creating Instances
+###
+def createVPCInstance(vpc):
+    pass
+    
+###
+## Updating Instances
+###    
+def updateSecurityGroup(securityGroup):
+    # TODO: Add the current IP address to the security group
+    pass
+    
+###
+## Fetching Instances
+###
+def getSecurityGroupFromInstance(ec2, instance):
+    attributes = ec2.get_instance_attribute(instances[0].id, "groupSet")
+    securityGroupId = attributes.get("groupSet")[0].id
+    retval = None
+    for securityGroup in ec2.get_all_security_groups()
+        if securityGroup.id == securityGroupId
+            retval = securityGroup
             break
     return retval
+    
+def getVPCInstance():
+    vpc = boto.vpc.connect_to_region(default_region)
+    instance = findExistingVPCInstance(vpc)
+    if instance is None:
+        instance = createVPCInstance(vpc)
+    
+    return instance
+    
+def getEC2Instance(ec2):
+    instance = findExistingEC2Instance(ec2)
+    
+    if instance is None:
+        # TODO: Create an instance
+        pass
+
+    if instance is not None:
+        securityGroup = getSecurityGroupFromInstance(ec2, instance)
+        updateSecurityGroup(securityGroup)
+    
+    return instance
+
+###
+## Starting Instances
+###
 
 #returns:
 # ec2ImageWait if the image is transitioning between states. The state should be checked again in a couple seconds.
@@ -71,15 +158,13 @@ def startInstance(ec2, instance):
 def startImageAndGetIP():
     ip = None
     ec2 = boto.ec2.connect_to_region(default_region)
-    instance = getProxyInstance(ec2)
-    if instance is None:
-        print "Couldn't find AWSProxy instance. Creating a new one"
-        # TODO: Create a new instance
+    instance = getEC2Instance(ec2)
     
     if instance is not None:
         startInstance(ec2, instance)
-    
-    return instance.public_dns_name
+        return instance.public_dns_name
+
+    return None
     
 ip = startImageAndGetIP()
 if ip is not None:
